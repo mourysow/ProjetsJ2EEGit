5d0d776a96ca522ebe2f9b5a4b9605e66b087cc4
mourysow <mamadou-oury.sow@outlook.com>
2016-10-27 00:19:43 +0200

    1 er commit projet Produit DB_Produit_JPA_STRUSTS

---
+++ TPST3/src/main/java/org/sid/service/ServiceSingleton.java
@@ -0,0 +1,17 @@
+package org.sid.service;
+
+import org.sid.dao.CatalogueDAOImpl;
+
+public class ServiceSingleton {
+  private static CatalogueServiceImpl service;
+  
+  static{
+	  CatalogueDAOImpl dao=new CatalogueDAOImpl();
+	  dao.initialisation();
+	  service=new CatalogueServiceImpl();
+	  service.setDao(dao);
+  }
+  public static ICatalogueService getService(){
+	  return service;
+  }
+}
